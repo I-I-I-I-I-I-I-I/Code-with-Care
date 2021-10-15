788a04332b41bb38ff3cdbd2320205acb36aa2c3
p c <patrick_c321@hotmail.co.uk>
2021-10-15 11:48:24 +0100

    yeo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+/*Colour Scheme:
+#008B74
+#00C2A8
+#4FFBDF
+#845EC2
+*/
+
+/*Screens
+
+  SplashScreen
+  AdminPage
+    - Student Access
+    - Resource Page
+  StudentsPage
+    -StudentPage1
+    -StudentPage2
+
+*/
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello from the other side
         </p>
         <a
           className="App-link"
