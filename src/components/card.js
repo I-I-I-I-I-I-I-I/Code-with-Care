28cdd4aa37431d992fd417e3dd9fed532782dd9b
p c <patrick_c321@hotmail.co.uk>
2021-10-30 23:27:05 +0100

    QOL changes

--- src/components/card.js
+++ src/components/card.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import '../App.css';
 import {Card , Button} from "react-bootstrap";
+import blank_profile from "../images/blank_profile.png"
 
 const card = () => {
 
 const studentList = [
-    {name : "Patrick" , desc : "LOREM SOMETHING YAYAYA" , image : "./logo512.png"},
-    {name : "Betty" , desc : "Tester 4335 quazimodo" , image : "./logo512.png"},
-    {name : "Carson" , desc : "Jingo Jango limbo lambo" , image : "./logo512.png"},
-    {name : "Ringo" , desc : "Zing zing azepp" , image : "./logo512.png"},
-    {name : "Bingo" , desc : "Not to be confused with Ringo" , image : "./logo512.png"},
-    {name : "Headphones" , desc : "Keeps my ears happy" , image : "./logo512.png"},
-    {name : "Jessie" , desc : "my dog" , image : "./logo512.png"},
-    {name : "Ringo" , desc : "another ringo, just a coincidence" , image : "./logo512.png"},
+    {name : "Patrick" , desc : "LOREM SOMETHING YAYAYA" , image : ""},
+    {name : "Betty" , desc : "Tester 4335 quazimodo" , image : ""},
+    {name : "Carson" , desc : "Jingo Jango limbo lambo" , image : ""},
+    {name : "Ringo" , desc : "Zing zing azepp" , image : ""},
+    {name : "Bingo" , desc : "Not to be confused with Ringo" , image : ""},
+    {name : "Headphones" , desc : "Keeps my ears happy" , image : ""},
+    {name : "Jessie" , desc : "my dog" , image : ""},
+    {name : "Ringo" , desc : "another ringo, just a coincidence" , image : ""},
   ]
 
 const creator = (card) => {
+    
+    if(card.image == "")
+        {
+            card.image = blank_profile
+        }
+    
     return(
     <Card style={{ width: '18rem' }}>
     <Card.Img variant="top" src= {card.image} />
