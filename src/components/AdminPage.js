27f2176921f7a9c29e5b0615a8abfc87178ae2cd
p c <patrick_c321@hotmail.co.uk>
2021-10-30 18:08:04 +0100

    Added a card component to interact with student page, stil WIP

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import "../App.css";
-import Card from "./card"
 
 function App() {
-
     return (
       <div className="App">
           
-            <h1 className="pageHeaders">Students</h1>
-            <div className = "cards">
-              <Card/>
-            </div>
+            <h1></h1>
 
       </div>
     );
   }
-
-
   
   export default App;
   
--- src/components/navbar/menuItems.jsx
+++
@@ -1,7 +0,0 @@
-const menuItems = [
-    {
-        title : 'Home',
-        url : '#',
-        cName : '#'
-    }
-]
