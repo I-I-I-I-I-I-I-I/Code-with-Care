553f6b6db204d05b8e6b7f9f6f286ab234425c6a
Cameron McKimm <cameronmckimm12345@gmail.com>
2021-11-04 22:34:44 +0000

    [FEATURE] Base student page has been created with some multimedia.

--- src/components/StudentPage.js
+++ src/components/StudentPage.js
@@ -1,14 +1,49 @@
 import React from 'react'
 import {BackButton} from "./BackButton";
+import ReactPlayer from "react-player";
+import {Card, Col, Container, Image, Row} from "react-bootstrap";
+import Picture from "../images/1200px-Circuit_elements.svg.png";
 
 function studentPage() {
     return (
         <div className="App">
-            <div style={{display:"flex"}}>
+            <div style={{display: "flex"}}>
                 <BackButton/>
             </div>
-            <h1>Basic Electronics</h1>
-
+            <Container>
+                <Row>
+                    <h1>Basic Electronics</h1>
+                </Row>
+                <Row>
+                    <ReactPlayer
+                        url="https://www.youtube.com/watch?v=uXr4lXYjXuU&ab_channel=TheOrganicChemistryTutor"
+                        controls={true}
+                        width='100%'
+                    />
+                </Row>
+                <Row style={{margin: "1.3em"}}>
+                    <Col md>
+                        <Card className="my-1" bg="primary" text="white">
+                            <Card.Body>
+                                <Card.Title>Lorem ipsum dolor.</Card.Title>
+                                <Card.Text>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus cumque debitis
+                                    dolore
+                                    eos esse fugiat harum, impedit, in ipsum modi officiis rem rerum sapiente sed
+                                    soluta!
+                                    Aut
+                                    dolorem dolorum earum expedita harum, laborum minima modi odio qui ullam? Ea ipsum
+                                    porro
+                                    vel.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md>
+                        <Image className="my-1" src={Picture} fluid rounded/>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
