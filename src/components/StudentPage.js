f4198b04a03ea9f46ffe42fd7ecf173fcc937583
Cameron McKimm <cameronmckimm12345@gmail.com>
2021-11-06 19:40:35 +0000

    [FEATURE] Text to Speach (TTS) is now working in the student page.

--- src/components/StudentPage.js
+++ src/components/StudentPage.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import {BackButton} from "./BackButton";
 import ReactPlayer from "react-player";
-import {Card, Col, Container, Image, Row} from "react-bootstrap";
+import {FaVolumeUp} from "react-icons/all";
+import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
 import Picture from "../images/1200px-Circuit_elements.svg.png";
+
+const speak = (message) => {
+    const speech = new SpeechSynthesisUtterance(message);
+    [speech.voice] = speechSynthesis.getVoices();
+    speechSynthesis.speak(speech);
+};
+
+const speakAllElements = () => {
+    let elements = document.getElementsByClassName("text");
+    let fullText = '';
+    for (let i = 0; i < elements.length; i++) {
+        fullText += elements[i].innerText + '\n';
+    }
+    speak(fullText);
+};
 
 function studentPage() {
     return (
@@ -25,17 +41,21 @@ function studentPage() {
                     <Col md>
                         <Card className="my-1" bg="primary" text="white">
                             <Card.Body>
-                                <Card.Title>Lorem ipsum dolor.</Card.Title>
-                                <Card.Text>
+                                <Card.Title>
+                                    Lorem ipsum dolor.
+                                </Card.Title>
+                                <Card.Text class="text">
                                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus cumque debitis
                                     dolore
                                     eos esse fugiat harum, impedit, in ipsum modi officiis rem rerum sapiente sed
                                     soluta!
-                                    Aut
-                                    dolorem dolorum earum expedita harum, laborum minima modi odio qui ullam? Ea ipsum
-                                    porro
-                                    vel.
                                 </Card.Text>
+                                <Card.Footer>
+                                    <Button variant="warning" onClick={() => speakAllElements()}
+                                            aria-label="Click this button to speak the text.">
+                                        <FaVolumeUp/> Speak text!
+                                    </Button>
+                                </Card.Footer>
                             </Card.Body>
                         </Card>
                     </Col>
