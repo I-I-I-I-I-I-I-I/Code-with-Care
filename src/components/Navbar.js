71860c7a36d4c3b1f098a68661bbc90e50f458e2
Cameron McKimm <cameronmckimm12345@gmail.com>
2021-11-09 00:19:29 +0000

    [BUG] Fixed the navbar to align to left.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,16 +8,15 @@ function App() {
     return (
         <div className="Nav">
             <Navbar variant="dark" bg="dark" expand="lg">
-                <Container>
+                <Container fluid>
                     <Link to = "/students">
-                    <Navbar.Brand className = "d-inline-block align-top"><img style = {{height : "2.5rem"}}src = {logo}></img></Navbar.Brand>
+                    <Navbar.Brand><img style={{height : "2.5rem"}} src = {logo}/></Navbar.Brand>
                     </Link>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="container-fluid">
-
+                        <Nav className="me-auto">
                             <Link to="/students">
                                 <Button href="#students"><BsPersonCircle style={{marginBottom : "0.2rem"}}/> Students</Button>
-
                             </Link>
 
                             {/* TEMPORARY LINKS */}
@@ -33,13 +32,12 @@ function App() {
                                 <Nav.Link href="#AdminHandler">Admin handler</Nav.Link>
                             </Link>
 
-
                         </Nav>
+                        <Link to="/adminLogin">
+                            <Button variant="danger" className="dangerButton"><HiLockClosed style={{marginBottom : "0.2rem"}}/> Admin
+                                Login</Button>
+                        </Link>
                     </Navbar.Collapse>
-                    <Link to="/adminLogin">
-                        <Button variant="danger" className="dangerButton"><HiLockClosed style={{marginBottom : "0.2rem"}}/> Admin
-                            Login</Button>
-                    </Link>
                 </Container>
             </Navbar>
         </div>
